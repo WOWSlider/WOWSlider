0feeb74809acf8c809f1ef4b74695ebf07660db1
wowslider <github@free-web.ws>
2013-01-24 14:46:39 +0400

    initial commit with sample

---
+++ sample/script.js
@@ -0,0 +1,1 @@
+jQuery("#wowslider-container").wowSlider({effect:"rotate",prev:"",next:"",duration:20*100,delay:20*100,width:580,height:212,autoPlay:true,stopOnHover:false,loop:false,bullets:true,caption:true,captionEffect:"slide",controls:true,logo:"",images:0});
